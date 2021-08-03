e7b36a6c6223bbefc37b322ccd4c9f7aafd207f7
YellowSmy <61616519+YellowSmy@users.noreply.github.com>
2021-08-03 11:09:09 +0900

    security change
    good

--- myapp/settings.py
+++ myapp/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','(django-insecure-fu9p#qwppzg^3^e3+$$e9&rq-c0o8spb#*5@jzp(0p$3@64m_p')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
